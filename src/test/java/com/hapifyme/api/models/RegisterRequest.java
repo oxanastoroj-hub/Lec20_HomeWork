2266c85e93c5107c8da7fbf2c2c35f4017fada90
oxanastoroj-hubil.com <oxanastoroj@gmail.com>
2026-01-25 14:38:22 +0200

    bug fix: curatarea codului. eliminarea codului nefolosit

--- src/test/java/com/hapifyme/api/models/RegisterRequest.java
+++ src/test/java/com/hapifyme/api/models/RegisterRequest.java
@@ -23,9 +23,9 @@ public class RegisterRequest {
         this.password = password;
     }
 
-    public String getFirst_name() {
-        return first_name;
-    }
+//    public String getFirst_name() {
+//        return first_name;
+//    }
 
     public void setFirst_name(String first_name){
         this.first_name = first_name;
